64104f9cab901725c101a77122290721f04ae88b
adonaifariasdev <oficialadonai@hotmail.com>
2021-11-06 04:07:37 -0400

    Adição da Prática de Classe

---
+++ Práticas/Pr22.py
@@ -0,0 +1,25 @@
+# Classe Bola: Crie uma classe que modele uma bola:
+#     Atributos: Cor, circunferência, material
+#     Métodos: trocaCor e mostraCor
+
+class CirculoPerfeito:
+    def __init__(self):
+        self.cor = 'Azul'
+        self.circuferencia = 4
+        self.material = 'Papel'
+
+    def mostra_cor(self):
+        return self.cor
+
+    def trocar_cor(self, cor):
+        self.cor = cor
+
+circulo_primeiro = CirculoPerfeito()
+circulo_segundo = CirculoPerfeito()
+circulo_segundo.trocar_cor('Amarelo')
+print(type(circulo_primeiro))
+print(circulo_primeiro is circulo_segundo)
+print(id(circulo_primeiro), circulo_primeiro.mostra_cor())
+print(id(circulo_segundo), circulo_segundo.mostra_cor())
+
+print(circulo_primeiro.cor, circulo_segundo.cor)
